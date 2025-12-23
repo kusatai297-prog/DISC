a00146093f1bec18e79c30900afff0947d69ce6b
kusatai297-prog <kusatai297@gmail.com>
2025-12-23 11:08:19 +0900

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,94 @@
+'use client'
+
+import React, { useState } from 'react';
+import { question } from './questions';
+import { useRouter } from "next/navigation";
+
+
+
+const Card = ({ question, selected, onSelect }) => {
+  return (
+    <div className="card py-10">
+      <h2 className="card-question text-center text-2xl font-medium pl-12 pr-6 py-6">
+        {question.number}.{question.content}
+      </h2>
+
+      <div className="card-answer text-center space-x-4">
+        {[1,2,3,4,5].map((num) => (
+          <button
+            key={num}
+            onClick={() => onSelect(num)}
+            className="border border-gray-400 px-8 py-6 rounded hover:bg-gray-100 active:scale-95"
+            style={{ backgroundColor: selected === num ? 'lightblue' : 'white' }}
+          >
+            {num}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+export default function Home() {
+  const router = useRouter();
+  const [answers, setAnswers] = useState<number[]>(Array(question.length).fill(0));
+
+  const handleAnswer = (index: number, num: number) => {
+    const newAnswers = [...answers];
+    newAnswers[index] = num;
+    setAnswers(newAnswers);
+  };
+
+
+  const payload = answers.map((ans,idx) => ({
+      number: idx+1,
+      answer: ans
+    }));
+  
+  const handlesend = async () => {
+    if (answers.some(ans => ans===0)){
+      alert("すべての質問に回答してください");
+      return ;
+    }
+    const response = await fetch("http://127.0.0.1:8000/answers",{
+    method: "POST",
+    headers: {"Content-type":"application/json"},
+    body: (JSON.stringify({answers:payload}))
+  })
+    const data = await response.json()
+    console.log(data)
+
+    if (data.status == "error"){
+      alert(data.message);
+    }
+    else{
+      router.push(`/result/${data.type}`)
+    }
+}
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center mb-8">
+        RISCタイプ診断
+      </h1>
+      <p className="text-lg text-center text-gray-700 leading-relaxed">
+      この診断ではあなたが思ったことをそのまま回答してください。<br />
+      5が最も当てはまり、1が当てはまらないというように回答してください。
+    </p>
+      {question.map((q, idx) => (
+        <Card 
+          key={q.number} 
+          question={q} 
+          selected={answers[idx]} 
+          onSelect={(num) => handleAnswer(idx, num)} 
+        />
+      ))}
+      <button 
+      className="border border-gray-400 px-4 py-2 rounded hover:bg-gray-100 active:scale-95"
+      onClick={handlesend}>
+        結果を見る
+      </button>
+    </>
+  );
+}
